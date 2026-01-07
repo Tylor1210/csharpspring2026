818208cd776fce772625cc3b581407fd1c9a1f71
Tylor C <100950093+Tylor1210@users.noreply.github.com>
2026-01-06 22:29:42 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWinFormsApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShowMessage_Click(object sender, EventArgs e)
+        {
+            //Class    Method   Parameters
+            MessageBox.Show("I am going to master C#");
+        }
+
+    }
+}
